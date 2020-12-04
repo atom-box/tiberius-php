c5d952c459f944eacd22d85c34e9ba0e3002bd88
Evan Genest <genest.1@osu.edu>
2020-12-04 12:37:12 -0600

    Second drill, doubleX, works.

--- five-drills.php
+++ five-drills.php
@@ -15,3 +15,33 @@ function frontTimes($inString, $n){
 echo(frontTimes("Chocolate", 2)); // "ChoCho"
 echo(frontTimes("Chocolate", 3)); // "ChoChoCho"
 echo(frontTimes("Abc", 3)); // "AbcAbcAbc"
+
+
+///////////////////////////////////////////////////
+echo("//////////////////////////////\n");
+
+// Given a string, return true if the first instance of "x" in the string is immediately followed by another "x".
+
+function doubleX($s){
+    $WANTED = 'x';
+    $firstFound = strpos($s, $WANTED);
+    $successOne = is_int($firstFound);
+    if ($successOne){
+        // echo('found first x');
+        $successTwo = $WANTED === substr($s, $firstFound + 1, 1);
+        $overallSuccess = $successOne && $successTwo;
+        
+        return $overallSuccess ? "yay\n": "boo\n" ;
+    }
+    return "false\n";
+}
+
+echo(doubleX("xx")); // true
+echo(doubleX("axxbb")); // true
+echo(doubleX("xxxxx")); // true
+echo(doubleX("axaxax")); // false
+echo(doubleX("Louis Capella")); // false
+echo(doubleX("3 Mr. DiMartino's children")); // false
+echo(doubleX("x")); // false
+echo(doubleX("XX")); // ???
+
