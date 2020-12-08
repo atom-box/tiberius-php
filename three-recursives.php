ae21810494f082999736be9bd89da89d0ebe0752
atom-box <atom_box@hotmail.com>
2020-12-08 17:16:16 -0600

    Loop over the chars in the string.

---
+++ three-recursives.php
@@ -0,0 +1,60 @@
+<?php
+// Solve the following three problems recursively
+
+/*
+  /$$    
+ /$$$$    
+|_  $$    
+  | $$    
+  | $$    
+  | $$    
+ /$$$$$$  
+|______/  
+        
+Given a string, return recursively a "cleaned" string where adjacent chars that 
+are the same have been reduced to a single char. So "yyzzza" yields "yza".
+*/
+
+function stringClean($s){
+    // todo FAIL DELIBERATELY NOT CHECK THE FINAL CHAR, RETURN IT NO MATTER WHAT todo
+    $i = 0;
+    $recentChar = [''];
+    while (strlen($s) > $i){
+        echo ("Looking at: ".substr($s, $i, 1)."\n");
+        $recentChar[] = substr($s, $i, 1);
+        // Set flag to T/F  and unload left side of queue
+        $justSaw = ( array_shift($recentChar) ===  substr($s, $i, 1));
+        // IF YOU JUST SAW THIS A MINUTE AGO 
+        //     i++;
+        // IF THIS IS UNFAMILIAR, RETURN JUST IT
+        $i++;
+    }
+
+}
+
+echo(stringClean("yyzzza")."\n"); // "yza"
+echo(stringClean("abbbcdd")."\n"); // "abcd"
+echo(stringClean("Hello")."\n"); // "Helo"
+          
+/*          
+  /$$$$$$ 
+ /$$__  $$
+|__/  \ $$
+  /$$$$$$/
+ /$$____/ 
+| $$      
+| $$$$$$$$
+|________/
+          
+          
+          
+  /$$$$$$ 
+ /$$__  $$
+|__/  \ $$
+   /$$$$$/
+  |___  $$
+ /$$  \ $$
+|  $$$$$$/
+ \______/ 
+        
+ */
