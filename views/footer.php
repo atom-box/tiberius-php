29f7c845ab2a8320bc10c51612f09e181f0048dd
atom-box <atom_box@hotmail.com>
2020-12-27 10:41:45 -0600

    Serve a random image into footer.

--- views/footer.php
+++ views/footer.php
@@ -1,4 +1,10 @@
 <footer>
-    <p>2020 @ndash; Cruz Azul</p>
-    <img src="resources/images/alvin1.png" alt="dancer from Alvin Ailey">
+    <p>2020 @ndash; Cruz Azul
+    <?php 
+    $imageCount = 4;
+    $i = random_int(1, $imageCount);
+    $imagePath = "resources/images/alvin{$i}.png";
+    ?>
+    <img src="<?= $imagePath ?>" alt="dancer from Alvin Ailey">
+    </p>
 </footer>
