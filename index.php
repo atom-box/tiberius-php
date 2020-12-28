6629da177f0b5fbecaca95a6142a65dc6af2de5c
atom-box <atom_box@hotmail.com>
2020-12-28 08:29:21 -0600

    Move challenge logic and HTML out into a view

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
             font-size: 3rem;
         }
 
-        div.codeholder {
+        article.codeholder {
             border: 2px solid yellow;
             width: 60%;
             border-radius: 0.3rem;
@@ -29,49 +29,12 @@
     <header>
         <h1>Challenges.</h1>
     </header>
-    <div class="codeholder">
-        <article>
-            <code>
-        <?php
-        echo "You have a red lottery ticket showing ints a, b, and c, each of which is 0, 1, or 2. If they are all the value 2, the result is 10. Otherwise if they are all the same, the result is 5. Otherwise so long as both b and c are different from a, the result is 1. Otherwise the result is 0.
-        <br><br>";
-        $answer = redTicket(2, 2, 2);
-        echo "
-        redTicket(2, 2, 2) → 10
-        <br>$answer<br>";
-        
-        $answer = redTicket(0, 0, 0);
-        echo "
-        redTicket(0, 0, 0) → 5
-        <br>$answer<br>";
-        
-        $answer = redTicket(2, 2, 1);
-        echo "
-        redTicket(2, 2, 1) → 0
-        <br>$answer<br>";
-        
-        function redTicket(int $a, int $b, int $c): int {
-            $is_null = 101.3;
-            // switch ($nums){
-                //     case : 
-                    //         return 33;
-                    // }
-                    $payoff = $is_null ?? 44;
-                    return $payoff;
-                }
-        ?>
 
-        <?php
-        require $_SERVER['DOCUMENT_ROOT'].'/Controllers/BadgeController.php';
-        $emojiBringer = new BadgeController();
-        $badge1 = $emojiBringer->showBadge(1);
-        $badge2 = $emojiBringer->showBadge(2);
-        $badge3 = $emojiBringer->showBadge(3);
-        $badge4 = $emojiBringer->showBadge(42);
-        ?>
-        <p>&#<?=$badge1  ?></p><p>&#<?=$badge2  ?></p><p>&#<?=$badge3  ?>;</p><p>&#<?=$badge4  ?>;</p>
-    </code>
-</article>
+
+    <article class="codeholder">
+    <?php require $_SERVER['DOCUMENT_ROOT'].'/views/chalkboard.php'; ?>
+        ////
+    </article>
 </div>
 <?php require $_SERVER['DOCUMENT_ROOT'].'/views/footer.php'; ?>
 
---
+++ views/chalkboard.php
@@ -0,0 +1,46 @@
+<code>
+
+<p>You have a red lottery ticket showing ints a, b, and c, each of which is 0, 1, or 2. If they are all the value 2, the result is 10. Otherwise if they are all the same, the result is 5. Otherwise so long as both b and c are different from a, the result is 1. Otherwise the result is 0.</p>
+
+
+<p>redTicket(2, 2, 2) → 10</p>
+<?php 
+$answer = redTicket(2, 2, 2);
+echo $answer;
+?>
+
+<p>redTicket(2, 2, 1) → 0</p>
+<?php 
+$answer = redTicket(2, 2, 1);
+echo $answer;
+?>
+
+<p>redTicket(0, 0, 0) → 5</p>
+<?php 
+$answer = redTicket(0, 0, 0);
+echo $answer;
+?>
+
+
+<?php 
+function redTicket(int $a, int $b, int $c): int {
+    $is_null = 101.3;
+    // switch ($nums){
+        //     case : 
+            //         return 33;
+            // }
+            $payoff = $is_null ?? 44;
+            return $payoff;
+}
+?>
+
+<?php
+require $_SERVER['DOCUMENT_ROOT'].'/Controllers/BadgeController.php';
+$emojiBringer = new BadgeController();
+$badge1 = $emojiBringer->showBadge(1);
+$badge2 = $emojiBringer->showBadge(2);
+$badge3 = $emojiBringer->showBadge(3);
+$badge4 = $emojiBringer->showBadge(42);
+?>
+<p>&#<?=$badge1  ?></p><p>&#<?=$badge2  ?></p><p>&#<?=$badge3  ?>;</p><p>&#<?=$badge4  ?>;</p>
+</code>
