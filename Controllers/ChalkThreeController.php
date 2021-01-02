06aab5db32e34d1cf22c060ae57b1d1ca19b280e
atom-box <atom_box@hotmail.com>
2021-01-02 06:34:46 -0600

    Rename controller.

--- Controllers/ChalkThreeController.php
+++ Controllers/ChalkThreeController.php
@@ -1,6 +1,6 @@
 <?php 
 
-class ArrayTwoController {
+class ChalkThreeController {
 
     public function describeChallenge(): string {
         return "Given an array of ints, return true if the value 3 appears in the array exactly 3 times, and no 3's are next to each other.";
