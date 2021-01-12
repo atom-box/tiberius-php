50322b1eb26ad465911f0cfb87422d6053694a03
atom-box <atom_box@hotmail.com>
2021-01-12 05:15:57 -0600

    Write namespace example.

---
+++ unrelated/file1.php
@@ -0,0 +1,14 @@
+<?php
+namespace Foo\Bar\subnamespace;
+
+const FOO = 1;
+function foo() {}
+class foo
+{
+    static function staticmethod() {}
+}
+?>
+
+
+// https://www.php.net/manual/en/language.namespaces.basics.php
+// see file2.php in same folder
