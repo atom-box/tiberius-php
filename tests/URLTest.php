17d5afed89d8fcdae09fe1a97c264ee480fd9590
atom-box <atom_box@hotmail.com>
2021-01-20 06:15:08 -0600

    Add French test.

--- tests/URLTest.php
+++ tests/URLTest.php
@@ -9,69 +9,6 @@ require 'Controllers/constants.php';
 require 'Controllers/URL.php';
 class URLtest extends TestCase
 {
-    public function testStringEndsAreNotDashed(){
-        $urlTools = new URL();
-        
-        // $originalString = '1. this string will be sluggified!! !'; 
-        $originalString = '-1 bad mf-'; 
-        $expectedResult = '1-bad-mf';
-        $output = $urlTools->sluggify($originalString);
-        $this->assertSame($expectedResult, $output);
-    }
-
-    public function testSixteenNastySymbolsAreIgnored(){
-        $urlTools = new URL();
-        
-        $originalString = 'dog(~!@#$%^&*()_+)?wing?'; 
-        $expectedResult = 'dog-wing';
-        $output = $urlTools->sluggify($originalString);
-        $this->assertSame($expectedResult, $output);
-    }
-
-
-    public function testTurkishLetters(){
-        $urlTools = new URL();
-        
-        $originalString = 'Türkçe, bir köprünün altında?'; 
-        $expectedResult = 'Türkçe-bir-köprünün-altında';
-        $output = $urlTools->sluggify($originalString);
-        $this->assertSame($expectedResult, $output);
-    }
-
-    public function testFetchFrenchDoctor(){
-        $urlTools = new URL();
-        $originalString = '1 médecin français, vite!';
-        $expectedResult = '1-médecin-français-vite';
-        $output = $urlTools->sluggify($originalString);
-        $this->assertSame($expectedResult, $output);
-    }   
-
-    public function testPhoneNumber(){
-        $urlTools = new URL();
-        
-        $originalString = '465-3958'; 
-        $expectedResult = '465-3958';
-        $output = $urlTools->sluggify($originalString);
-        $this->assertSame($expectedResult, $output);
-    }
-
-    public function testInputIsEmptyString(){
-        $urlTools = new URL();
-        $originalString = '';
-        $expectedResult = '';
-        $output = $urlTools->sluggify($originalString);
-        $this->assertSame($expectedResult, $output);
-    }
-
-    public function testInputIsNull(){
-        $urlTools = new URL();
-        $originalString = null;
-        $expectedResult = '';
-        $output = $urlTools->sluggify($originalString);
-        $this->assertSame($expectedResult, $output);
-        // TODO CHANGE TEST TO does not explode instead
-    }
-
 
  /**
   * @param string $originalString
@@ -81,7 +18,9 @@ class URLtest extends TestCase
   */
 
   public function testSluggifyReturnsSluggifiedString( $originalString, $expectedString){
-      //
+      $urlTools = new URL();
+      $output = $urlTools->sluggify($originalString);
+      $this->assertSame($expectedString, $output);
   }
 
   public function provideSlugging(){
@@ -89,10 +28,12 @@ class URLtest extends TestCase
         array('-1 bad mf-', '1-bad-mf'),
         array('dog(~!@#$%^&*()_+)?wing?', 'dog-wing'),
         array('Türkçe, bir köprünün altında?', 'Türkçe-bir-köprünün-altında'),
+        array('', ''),
+        array(null, ''),
+        array('(313) 465-3958', '313-465-3958'),
+        array('1 médecin français, vite!', '1-médecin-français-vite'),
       );
   }
-
-
 }
 
 /**
@@ -104,4 +45,3 @@ class URLtest extends TestCase
  * 5) The sibling to your containing folder does not get a double dot.  The folder path naming assumes a starting point of the damn project root. So if you are in root/BROTHER, you call with REQUIRE 'SISTER/url.php'.
  * 
  */
-
