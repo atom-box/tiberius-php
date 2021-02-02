352f8752fd4f201506cf79e9cb7f20732b95e66e
atom-box <atom_box@hotmail.com>
2021-02-02 05:58:29 -0600

    Refactor function name to justAllCaps.

--- Controllers/SED.php
+++ Controllers/SED.php
@@ -3,14 +3,23 @@ namespace tiberius\tests;
 
 interface arbitraryname {
     public function firstfix(string $dirty): string;
+    public function justAllCaps(string $untested): $string;
 }
 
 class SED
 {
     public function firstfix($dirty)
     {
-
+        
         return $cleaned;
+    }
+    
+    public function justAllCaps(string $untested): string{
+        if(true){
+            $approved = $untested;
+            return $approved;
+        }
+        return ' burp '; // returning null or false would be nice
     }
 }
 
