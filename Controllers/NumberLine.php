68fe509e9f0a932b2c2d8c3ecff3ab7a2272efe7
atom-box <atom_box@hotmail.com>
2021-02-05 06:22:18 -0600

    Stub in NOTALONE.

--- Controllers/NumberLine.php
+++ Controllers/NumberLine.php
@@ -2,6 +2,7 @@
 
 interface checks {
     public function gapSame(int $a, int $b, int $c): string;
+    public function notAlone(array $a, int $n): string; 
 }
 
 class NumberLine implements checks {
@@ -14,8 +15,16 @@ class NumberLine implements checks {
         if ($small - $medium === $medium - $large){
             return 'toot your lanyard';
         }
-
+        
         return 'it\'s nahhht Scottish';
+    }
+
+    public function notAlone(array $a, int $n): string{
+        // some code here
+        // var_dump($a);
+
+        $joined = implode(' ', $a);
+        return $joined;
     }
 }
 
@@ -26,8 +35,25 @@ echo "\n".$tool->gapSame(4, 6, 2); // true
 echo "\n".$tool->gapSame(4, 6, 3); // false
 echo "\n\n";
 
+
+echo "\n".$tool->notAlone([1, 2, 3], 2)."\n"; // [1, 3, 3]
+echo "\n".$tool->notAlone([1, 2, 3, 2, 5, 2], 2)."\n"; // [1, 3, 3, 5, 5, 2]
+echo "\n".$tool->notAlone([3, 4], 3)."\n"; // [3, 4]
+
 /*
+GAPSAME FUNCTION
 Given three ints, a b c, one of them is small, one is medium and one is large. 
 Return true if the three values are evenly spaced, so the difference between 
 small and medium is the same as the difference between medium and large.
 */
+
+/**
+ NOTALONE FUNCTION
+ *We'll say that an element in an array is "alone" if there are values
+  before and after it, and those values are different from it. Return 
+  a version of the given array where every instance of the given value 
+  which is alone is replaced by whichever value to its left or right \
+  is larger.
+
+
+ */
